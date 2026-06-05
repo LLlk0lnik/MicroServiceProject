0573b60c234e920ee7151e0ca08ca4f2f8fce76a
LLlk0lnik <94215708+LLlk0lnik@users.noreply.github.com>
2026-06-05 06:52:02 +0300

    edit_dockerfile

--- ms_auth/src/config.py
+++ ms_auth/src/config.py
@@ -12,9 +12,10 @@ class Settings(BaseSettings):
     db_user: str
     db_password: SecretStr
     debug: bool = False
+    db_url: str
 
-    def db_url(self) -> str:
-        return f"postgresql+asyncpg://{self.db_user}:{self.db_password.get_secret_value()}@{self.db_host}:{self.db_port}/{self.db_name}"
+    # def db_url(self) -> str:
+    #     return f"postgresql+asyncpg://{self.db_user}:{self.db_password.get_secret_value()}@{self.db_host}:{self.db_port}/{self.db_name}"
 
     model_config = SettingsConfigDict(
         env_file=f".env.{os.getenv('APP_ENV', 'stg')}",
--- ms_auth/src/core/session.py
+++ ms_auth/src/core/session.py
@@ -10,14 +10,14 @@ engine = create_async_engine(url=DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-class Base(AsyncAttrs, DeclarativeBase):
-    __abstract__ = True
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
-    update_at: Mapped[datetime] = mapped_column(
-        server_default=func.now(), onupdate=func.now()
-    )
+# class Base(AsyncAttrs, DeclarativeBase):
+#     __abstract__ = True
+#
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+#     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+#     update_at: Mapped[datetime] = mapped_column(
+#         server_default=func.now(), onupdate=func.now()
+#     )
 
     # @declared_attr.directive
     # def __tablename__(cls) -> str:
